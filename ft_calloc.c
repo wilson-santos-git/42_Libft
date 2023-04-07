181f8ab963c485e83cbeb3be56a8a22d9e765c98
Wilson Santos <wilsontsantos1998@gmail.com>
2023-04-07 19:16:28 +0100

    nice >D

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: wteles-d <wteles-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 19:21:49 by wteles-d          #+#    #+#             */
-/*   Updated: 2023/04/03 16:54:25 by wteles-d         ###   ########.fr       */
+/*   Updated: 2023/04/07 18:56:44 by wteles-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-
+	
 }
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,47 @@
 /*   By: wteles-d <wteles-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 18:47:42 by wteles-d          #+#    #+#             */
-/*   Updated: 2023/04/07 18:47:42 by wteles-d         ###   ########.fr       */
+/*   Updated: 2023/04/07 19:12:49 by wteles-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
+static	int	my_stringl(const char	*len)
+{
+	int	l;
+
+	l = 0;
+	while (len[l] != '\0')
+		l++;
+	return (l);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	sdst;
+	size_t	srcl;
+	size_t	i;
+	size_t	j;
+
+	sdst = my_stringl(dst);
+	srcl = my_stringl((char *)src);
+	i = 0;
+	j = 0;
+	while (dst[i] != '\0')
+	{
+		i++;
+	}
+	if (size <= sdst || size == 0)
+	{
+		return (size + srcl);
+	}
+	while (src[j] != '\0' && j < size - sdst - 1)
+	{
+		dst[i] = src[j];
+		i++;
+		j++;
+	}
+	dst[i] = '\0';
+	return (sdst + srcl);
+}
