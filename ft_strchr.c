1ec1570c0dcb6d3a33d3f37f71b12ebc383969d4
Wilson Santos <wilsontsantos1998@gmail.com>
2023-04-18 16:03:40 +0100

    .

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,18 +6,21 @@
 /*   By: wteles-d <wteles-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 17:33:24 by wteles-d          #+#    #+#             */
-/*   Updated: 2023/04/13 14:33:36 by wteles-d         ###   ########.fr       */
+/*   Updated: 2023/04/18 15:43:00 by wteles-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stddef.h>
+#include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
 {
 	int	i;
 
 	i = 0;
-	while (s && s[i] != '\0' && s[i] != c)
-	{
+	while (s[i] != '\0' && s[i] != (char)c)
 		i++;
-	}
-	return ((char *)s + i);
+	if (s[i] == (char)c)
+		return ((char *)s + i);
+	return (NULL);
 }
